bc86dfacdbce78bd40a001b49ef8c595c1ed4640
tehpwny <zomfgcookie@gmail.com>
2013-08-30 20:39:38 +0200

    Prototype modulation

--- bangz/LocalMod.py
+++ bangz/LocalMod.py
@@ -3,13 +3,15 @@
 
 
 class BangModule:
+
     def __init__(self):
         self.bangz = {
             'hello': self.hello,
-            'sayderp': self.derpity
+            'sayderp': self.derpity,
+            'op': self.op
             }
 
-    def hello(self,args,feedback=None):
+    def hello(self, args, feedback=None):
         retour = []
         for dude in args:
             retour.append('PRIVMSG ' + feedback.split('!')[0] + ' :' + ' hello ' + dude)
@@ -22,5 +24,11 @@ class BangModule:
             derp += 'derp'
         derp += '!'
         retour.append('PRIVMSG ' + feedback.split('!')[0] + ' :' + derp)
+        return retour
+
+    def op(self, args, feedback=None):
+        retour = []
+        retour.append('MODE ' + feedback.split('!')[0] + ' +o ' + feedback.split('!')[1])
+        print(retour)
         return retour
 
