efb58fde8f9f8ae695194859b142c2072eb952af
Thomas 'Winter' Sauze <sauze.thomas@gmail.com>
2013-10-12 17:36:50 +0000

    Add basic files.

--- IRCBoat.py
+++ IRCBoat.py
@@ -2,66 +2,70 @@ from IRCConnect import IRCConnect
 
 class IRCBoat:
   def __init__(self, irc_stream):
-    """IRCConnect
-    """
     self.irc_stream = irc_stream
-    self.nick(self.irc_stream.nick)
-    self.user(self.irc_stream.ident, self.irc_stream.host, self.irc_stream.realname)
+    self.irc_stream.ssl_connect()
+    self.irc_stream.send('NICK '+self.irc_stream.nick)
+    self.irc_stream.send('USER '+self.irc_stream.ident+' '+self.irc_stream.host+' '
+      +self.irc_stream.host+' :'+self.irc_stream.realname)
+    self.irc_stream.send('JOIN #testbot2')
+    self.irc_stream.send('PRIVMSG #testbot2 :Hello World')
+    while 1:
+      self.listen_input()
 
-  
   def listen_input(self):
-  """Get the content of the stream.
-  """
+    """Get the content of the stream.
+    """
     lines = self.irc_stream.read_stream()
     if lines != '':
-      print(stream)
+      print(lines)
       for line in lines:
         msg = line.split(' ')
-        # insert analytics functions.
+        #Insert analytics functions here.
 
-  ################################
-  # BASIC IRC'S COMMANDS         #
-  ################################
+  #######################
+  # ANALYTICS FUNCTIONS #
+  #######################
+  
+  def parser(self, stream_content):
+    """str
+    Parse the text and use functions for
+    each case.
+    """
+
+  ########################
+  # IRC BASIC'S COMMANDS #
+  ########################
+
   def nick(self, nick):
     """str
     Set the nick of the bot.
     """
-    self.irc_stream.send('NICK ' + nick)
-
-  def user(self, ident, host, realname ):
-    """str, str, str,
-    Authing on the IRC server.
+    self.irc_stream.send('NICK '+nick)
+  def user(self, ident, host, realname):
+    """str, str, str
+    Auth on the server.
     """
-    self.irc_stream.send('USER ' + ident + ' ' + host + ' ' 
-      + host + ' :' + realname)
-
+    self.irc_stream.send('USER '+user+' '+host+' '+host+' :'+realname)
   def pong(self, ping):
     """str
-    Address of the server.
+    Answer to the server's ping.
     """
-    self.irc_stream.send('')
-
+    self.irc_stream.send('PONG '+ping)
   def join(self, chan):
     """str
-    Join chan.
+    Join the chan.
     """
-    self.irc_stream.send('')
-
-  def message(self, dest, message):
+    self.irc_stream.send('JOIN '+chan)
+  def message(self, dest, msg):
     """str, str
-    Send a private message to dest.
+    Send the message to the dest.
+    Dest can be a channel or an other user.
     """
-    self.irc_stream.send('')
-
-  def topic(self, chan, topic):
-    """str, str
-    Modify the topic of the chan.
-    """
-    self.irc_stream.send('')
-
+    self.irc_stream.send('PRIVMSG '+dest+' :'+msg)
   def set_mode(self, chan, mode, nick=None):
     """str, str, str
-    Modify the mode of the selected chan.
+    Change the mod of the channel.
     """
-    self.irc_stream.send('')
- 
+    self.irc_stream.send('MODE '+mode+' '+nick)
+
+
--- IRCConnect.py
+++ IRCConnect.py
@@ -1,53 +1,57 @@
-import socket, ssl
-import os, time
+import socket, ssl, os, time
 
 class IRCConnect:
-	def __init__(self, host, port, nick, ident, realname):
-		self.host = host
-		self.port = port
-		self.nick = nick
-		self.ident = ident
-		self.realname = realname
+  """Improve the connection between the bot and IRC
+  """
 
-		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.buffer = ''
-		
-	def ssl_connect(self):
-		self.socket.connect((self.host, self.port))
-		self.socket = ssl.wrap_socket(self.socket, keyfile=None,
-				certfile=None,server_side=False,
-				do_handshake_on_connect=False,
-				suppress_ragged_eofs=True)
-		self.raw_irc_command('NICK ' + self.nick )
-		self.raw_irc_command('USER ' + self.ident + ' ' + self.host + ' ' + self.host +
-				' :' + self.realname )
+  def __init__(self, host, port, nick, ident, realname):
+    """str, int, str, str, str
+    host: irc.pastafarai.me
+    port: 1337
+    nick: Your nickname.
+    ident: Your id.
+    realname: "Lastname Firstname"
+    """
+    self.host = host
+    self.port = port
+    self.nick = nick
+    self.ident = ident
+    self.realname = realname
+    self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.stream_content = ''
 
-	def raw_irc_command(self, cmd):
-		self.send(str(cmd)+'\r\n')
-	
-	def send(self, data):
-		self.socket.send(data.encode('utf-8'))
-	
-	#Work on the encodage: make it in unicode for the special characters.
-	def read_stream(self): #Rework that.
-		self.buffer = ''
-		self.buffer = self.socket.recv(2040)
-		print self.buffer
-		if self.buffer != '':
-			self.buffer = self.buffer.split("\n")
-			self.buffer.pop()
-			pass
-		return self.buffer
+  def ssl_connect(self):
+    """Initialize the connect to IRC in SSL.
+    """
+    self.socket.connect((self.host, self.port))
+    self.socket = ssl.wrap_socket(self.socket, keyfile=None,
+      certfile=None, server_side=False,
+      do_handshake_on_connect=False,
+      suppress_ragged_eofs=True)
+  
+  def raw_irc_command(self, cmd):
+    """str->str
+    cmd: The command to send.
+    """
+    return str(cmd)+'\r\n'
 
-	def listen_input(self):
-		lines = self.read_stream()
-		if lines == '':
-			return
-		for line in lines:
-			msg = line.split(' ')
-			if msg[0] == 'PING':
-				self.raw_irc_command('PONG ' + msg[1].split(":")[1])
-			if msg[1] == 'PRIVMSG':
-				self.raw_irc_command('PRIVMSG #testbot :msg send by' + msg[0])
+  def send(self,cmd):
+    """str
+    cmd: The command rawed to send.
+    """
+    cmd = self.raw_irc_command(cmd)
+    self.socket.send(cmd.encode('utf-8'))
+
+  def read_stream(self):
+    """->str
+    Return the content of the current stream.
+    """
+    self.stream_content = ''
+    self.stream_content = self.socket.recv(2040)
+    if self.stream_content != '':
+      self.stream_content = self.stream_content.split('\n')
+      self.stream_content.pop()
+      pass
+    return self.stream_content
 
 
