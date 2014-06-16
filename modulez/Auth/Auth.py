6bbaa9499d8bda96d94612a23fcb3c074aa34076
tehpwny <pwny@lebib.org>
2014-06-16 14:11:01 +0200

    readme update

---
+++ modulez/Auth/Auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+
+
+
+# attr :
+#
+#
+# func :
+#
+# login, adduser, remuser, moduser
+#
+#
+#
+Class Auth():
+    def __init__(self, boat):
+        self.bangz = {
+            }
+        self.boat = boat
