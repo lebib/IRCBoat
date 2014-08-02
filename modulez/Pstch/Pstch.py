16f1dfeef59afd1d56617b21506e9e486bb6ffa6
tehpwny <pwny@lebib.org>
2014-08-02 06:39:24 +0200

    fu

---
+++ modulez/Pstch/Pstch.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+#from IRCBoat import level
+from IRCBoat import IRCBoat
+
+class Module:
+    def __init__(self, boat):
+        self.boat = boat
+
+class Prototype(Module):
+
+    def say(self, dst, source, argz):
+        text = ''
+        for arg in argz:
+            text = text + ' ' + arg
+        self.boat.msg(dst, text)
+
+    def op(self, dst, source, argz):
+        self.boat.set_mode(dst, '+o', source)
+
+
+    def join(self, dst, source, argz):
+        self.boat.join(argz[0])
+
+    def topic(self, dst, source, argz):
+        topic = ''
+        for arg in argz:
+            topic = topic + ' ' + arg
+        self.boat.topic(dst, topic)
---
+++ modulez/Pstch/__init__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+#from IRCBoat import level
+
+class BaseIRC():
+
+    def __init__(self, boat):
+        self.bangz = {
+            'say': self.say,
+            'op': self.op,
+            'join': self.join,
+            'topic': self.topic
+            }
+        self.boat = boat
+
+    def say(self, dst, source, argz):
+        text = ''
+        for arg in argz:
+            text = text + ' ' + arg
+        self.boat.msg(dst, text)
+
+
+
+    def op(self, dst, source, argz):
+        self.boat.set_mode(dst, '+o', source)
+
+    def join(self, dst, source, argz):
+        self.boat.join(argz[0])
+
+    def topic(self, dst, source, argz):
+        topic = ''
+        for arg in argz:
+            topic = topic + ' ' + arg
+        self.boat.topic(dst, topic)
